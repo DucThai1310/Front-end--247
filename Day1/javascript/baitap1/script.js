7e9adbb09a20bbd87aa846de62effbb2bbe17b89
Thien Dong <datistpham@gmail.com>
2023-05-18 11:10:57 +0700

    bai1js

---
+++ Day1/javascript/baitap1/script.js
@@ -0,0 +1,16 @@
+document.write("<h1>The area and Perimeter App </h1>")
+let length = parseFloat(prompt("Enter Length:"));
+document.write("Length:", length, "<br>");
+let width = parseFloat(prompt("Enter width:"));
+document.write("Width: ", width, "<br>")
+document.write("<br>");
+let area = length * width;
+document.write("Area = ", area, "<br>");
+let perimeter = 2 * length + 2 * width;
+document.write("Perimeter =", perimeter, "<br>");
+document.write("<br>");
+document.write("<br>");
+alert("area = " + area);
+alert("Perimeter = " + perimeter);
+document.write("Perimeter =", perimeter, "<br>");
+document.write("Thank for using The Area and Perimeter application.")
